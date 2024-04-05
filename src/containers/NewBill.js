0a0455c5e87e368c7db556f87c52b984ad15de85
germainb <gbld@mailo.com>
2024-04-05 13:17:55 +0200

    [Bug Hunt] - Bills - files format

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -13,53 +13,85 @@ export default class NewBill {
     this.fileUrl = null
     this.fileName = null
     this.billId = null
+    this.isValidFormat = false
     new Logout({ document, localStorage, onNavigate })
   }
-  handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
-  }
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    const email = JSON.parse(localStorage.getItem("user")).email
+  handleChangeFile = (e) => {
+    e.preventDefault();
+  
+    const file = e.target.files[0]; // Récupère le fichier sélectionné par l'utilisateur
+    const fileName = file.name; // Récupère le nom du fichier
+  
+    // Vérifie si le format du fichier est valide (jpg, jpeg, png)
+    const isValidFormat = /\.(jpg|jpeg|png)$/i.test(fileName);
+    this.isImgFormatValid = isValidFormat;
+  
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+  
+    if (!isValidFormat) {
+      // Si le format du fichier n'est pas valide
+      fileInput.value = "";
+      fileInput.classList.add("is-invalid");
+      fileInput.classList.remove("blue-border");
+      alert("Le format de votre fichier n'est pas pris en charge." + "\n" + "Seuls les .jpg, .jpeg, .png sont acceptés.");
+    } else {
+      // Si le format du fichier est valide
+      fileInput.classList.remove("is-invalid");
+      fileInput.classList.add("blue-border");
+  
+      const formData = new FormData();
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      formData.append("file", file);
+      formData.append("email", email);
+      this.formData = formData;
+      this.fileName = fileName;
+    }
+  };
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+  
+    // Récupération de l'email de l'utilisateur à partir des données stockées localement
+    const email = JSON.parse(localStorage.getItem("user")).email;
+  
+    // Création de l'objet bill à partir des valeurs des champs de formulaire
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
       fileUrl: this.fileUrl,
       fileName: this.fileName,
-      status: 'pending'
+      status: "pending",
+    };
+  
+    // Vérification si le format de l'image est valide
+    if (this.isImgFormatValid) {
+      // Téléchargement de l'image et création de la facture uniquement si le format de l'image est valide
+      this.store.bills().create({
+        data: this.formData,
+        headers: {
+          noContentType: true,
+        },
+      })
+      .then(({ fileUrl, key }) => {
+        // Mise à jour de l'URL de l'image téléchargée et de l'ID de la facture
+        this.billId = key;
+        this.fileUrl = fileUrl;
+      })
+      .then(() => {
+        // Mise à jour de la facture et redirection vers la page des factures
+        this.updateBill(bill);
+        this.onNavigate(ROUTES_PATH["Bills"]);
+      })
+      .catch((error) => console.error(error));
     }
-    this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
-  }
+  };
 
   // not need to cover this function by tests
   updateBill = (bill) => {
