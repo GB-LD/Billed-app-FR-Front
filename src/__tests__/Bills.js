1fa48d129abd0b6ce2edbc2cabed9b1acc23f735
germainb <gbld@mailo.com>
2024-04-15 17:53:40 +0200

    complete expect composant views/Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -26,8 +26,9 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(document.querySelector("#layout-icon1").classList.contains("active-icon"))
     })
+    
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
